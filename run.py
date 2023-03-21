b47dc84dbf77ab46a802dfddae646b3effc0b51b
Rubem Janoni <rubemjanoni@gmail.com>
2023-03-21 00:23:10 +0000

    Retrieve latest stock numbers from spreadsheet

--- run.py
+++ run.py
@@ -1,4 +1,7 @@
 import gspread
+from pprint import pprint
+
+
 from google.oauth2.service_account import Credentials
 
 SCOPE = [
@@ -13,9 +16,7 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('love_sandwiches')
 
-
-def get_sales_data():
-         
+def get_sales_data():         
     """
     Get sales figures input from the user.
     Run a while loop to collect a valid string of data from the user
@@ -66,11 +67,36 @@ def update_sales_worksheet(data):
     sales_worksheet.append_row(data)
     print("Sales worksheet updated sucessfully.")
 
+def calculate_surplus_data(sales_row):
+    """
+    Compare sales with stock and calculate the surplus for each item type.
+    The surplus is defined as the sales figure subtracted from the stock:
+    - Positive surplus indicates waste
+    - Negative surplus indicates extra made when stock was sold out.
+    """
+    print("Calculating surplus data...\n")
+    stock = SHEET.worksheet("stock").get_all_values()
+    stock_row = stock[-1]
+    print(stock_row)  
+
+def main():
+    """
+    Run all program functions
+    """
+    data = get_sales_data()
+    sales_data = [int(num) for num in data]
+    update_sales_worksheet(sales_data)
+    calculate_surplus_data(sales_data)
+    
+
+print("Welcome to Love Sandwiches automation")
+
+main()    
 
 
-data = get_sales_data()
-sales_data = [int(num) for num in data]
-update_sales_worksheet(sales_data)
+
+
+
 
 
 
